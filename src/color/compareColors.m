b9b37c91e0ffeb0d61fa505694a5d9cfbd9b2138
Brent Munsell <munsellb@manaris-i5.cs.cofc.edu>
2016-05-12 10:08:54 -0400

    changed arguments

--- src/color/compareColors.m
+++ src/color/compareColors.m
@@ -1,4 +1,4 @@
-function sim=compareColors(c1,c2)
+function sim=compareColors( A, B )
   %% compare two color files using the CIE76 color difference metric
   %% c1 and c2 are paths to color file
   %% i.e. /:pillId/color.png
@@ -8,19 +8,22 @@ function sim=compareColors(c1,c2)
 
   
   % load images
-  c1 = rgb2lab(imread(c1));
-  c2 = rgb2lab(imread(c2));
+  %c1 = rgb2lab(imread(c1));
+  %c2 = rgb2lab(imread(c2));
 
+error( nargchk(2,2,nargin) );
 
+c1 = rgb2lab( imread( [ A.path '/color.jpg' ] ) );
+c2 = rgb2lab( imread( [ B.path '/color.jpg' ] ) );
 
-  % split color channels for images
-  L1 = c1(:,:,1);
-  a1 = c1(:,:,2);
-  b1 = c1(:,:,3);
-  L2 = c2(:,:,1);
-  a2 = c2(:,:,2);
-  b2 = c2(:,:,3);
+% split color channels for images
+L1 = c1(:,:,1);
+a1 = c1(:,:,2);
+b1 = c1(:,:,3);
+L2 = c2(:,:,1);
+a2 = c2(:,:,2);
+b2 = c2(:,:,3);
 
-  %% CIE LAB FORMULA 197*
-  sim = 1 - sqrt((L1-L2).^2 + (a1-a2).^2 + (b1-b2).^2) / 255;
-  sim = sim(1);
+%% CIE LAB FORMULA 197*
+sim = 1 - sqrt((L1-L2).^2 + (a1-a2).^2 + (b1-b2).^2) / 255;
+sim = sim(1);
