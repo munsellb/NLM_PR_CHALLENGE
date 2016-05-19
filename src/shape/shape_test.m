3acaccebb58c41491798e837a5c8c36b95a3b9db
Brent Munsell <munsellb@manaris-i5.cs.cofc.edu>
2016-05-19 15:30:46 -0400

    added new features and files

--- src/shape/shape_test.m
+++ src/shape/shape_test.m
@@ -2,10 +2,16 @@ function shape_test()
 
 num_test_images = 6;
 
-for i=1:num_test_images,
+% I=[0:200:4000];
+% i=I(15);
+
+% I=[0:1:20];
+% i=I(19);
+
+for i=1151:100:1961, %200:4000, %num_test_images,
     
-    test_img = ['test' num2str(i) '.jpg' ];
+    test_img = ['proc/dc/' num2str(i) '/' num2str(i) '.jpg' ];
     
-    shape( test_img );
+    shape( 'DC', test_img );
     
 end;
