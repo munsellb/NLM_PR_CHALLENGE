e929fe881b1d80cf22817e3ac4d46074e103ef14
Brent Munsell <munsellb@manaris-i5.cs.cofc.edu>
2016-05-23 15:30:40 -0400

    completed main script

--- showmethepills.m
+++ showmethepills.m
@@ -4,3 +4,14 @@ setmenv();
 
 orgImgs('DR','data','proc');
 orgImgs('DC','data','proc');
+
+procImgs('DR','proc');
+procImgs('DC','proc');
+
+opts.range = [1 5000];
+opts.save_files = true;
+opts.href = 1;
+
+classify( 'proc', 'DR', 'DC', opts );
+
+generateMR( 'proc', 'ShowMeThePills' );
