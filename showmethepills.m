2a89eb680cc34fe519befbc2c5dd4571d32dc3fa
Workstation2 <workstation2@localhost.localdomain>
2016-05-24 10:08:58 -0400

    updated for two input arguments

--- showmethepills.m
+++ showmethepills.m
@@ -1,17 +1,46 @@
-function showmethepills()
+function showmethepills( ref_dir, con_dir )
+%
+%	Usage: showmethepills( ref_dir, con_dir)
+%
+%
+%
+%
+%
+%
 
-setmenv();
+if nargin ~= 2,
+    
+    fprintf('Application requires two input arguments\n\n');
+    fprintf('Usage: showmethepills( ref_dir, con_dir)\n')
+    fprintf('       ref_dir = path to reference image folder\n');
+    fprintf('       con_dir = path to consumer image folder\n');
+    fprintf('Note: both arguments are strings\n\n');
+    
+    return;
+    
+end;
 
-orgImgs('DR','data','proc');
-orgImgs('DC','data','proc');
+fprintf('-----------------------------------------------\n');
+fprintf('[Step 1]: Executing Application\n');
 
+fprintf('[Step 2]: Organizing the reference pill images\n');
+orgImgs( 'DR', ref_dir, 'proc' );
+
+fprintf('[Step 2]: Organizing the consumer pill images\n');
+orgImgs( 'DC', con_dir, 'proc' );
+
+fprintf('[Step 3]: Processing the reference pill images\n');
 procImgs('DR','proc');
+
+fprintf('[Step 3]: Processing the consumer pill images\n');
 procImgs('DC','proc');
 
 opts.range = [1 5000];
 opts.save_files = true;
 opts.href = 1;
 
+fprintf('[Step 4]: Performing pill classification\n');
 classify( 'proc', 'DR', 'DC', opts );
 
+fprintf('[Step 5]: Creating MR CSV file\n');
 generateMR( 'proc', 'ShowMeThePills' );
